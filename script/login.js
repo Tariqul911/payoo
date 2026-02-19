7291c5ca442f4c02d0e43523b1769a423ca38564
Tariqul911 <zxtariqul911@gmail.com>
2026-02-19 21:33:40 +0600

    Implement Login Logic and nevigate to Homepage

--- script/login.js
+++ script/login.js
@@ -1,1 +1,33 @@
-console.log('connected');
+// 1.get the mobile number input
+    // 2.get the pin input
+    // 3.match pin & mobile number
+    // 3-1. true :::>> alert> homepage
+    // 3-2. false :::>> alert> return
+
+
+document.getElementById('login-btn').addEventListener('click', function(){
+    // 1.get the mobile number input
+    const numberInput = document.getElementById('input-number');
+    const contactNumber = numberInput.value;
+    console.log(contactNumber);
+
+    // 2.get the pin input
+    const inputPin = document.getElementById('input-pin');
+    const pinNumber = inputPin.value;
+    console.log(pinNumber);
+
+    // 3.match pin & mobile number
+    if(contactNumber == '01911296716' && pinNumber == '1234'){
+        // 3-1. true :::>> alert> homepage
+        alert('Login Success');
+
+        window.location.assign('/home.html');
+
+    }
+    else{
+        // 3-2. false :::>> alert> return
+        alert('Login Failed');
+        return;
+    }
+
+})
