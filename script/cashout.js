7f293202c3813c0687bd4227b0e395ba0e048cc7
Tariqul911 <zxtariqul911@gmail.com>
2026-02-20 11:07:35 +0600

    design cashout form and logical implimentation

---
+++ script/cashout.js
@@ -0,0 +1,9 @@
+document.getElementById('cashout-btn').addEventListener('click', function(){
+    // 1. Get the agent Number & validate
+    // 2. get the amount, validate, convert to Number
+    // 3. get the current Balance, validate, convert to Number
+    // 4. Calculate new Balance
+    // 5. Get the pin and verify
+    // 5-1 true :: show an alert > set New Balance
+    // 5-1 false :: show an error alert > return
+})
